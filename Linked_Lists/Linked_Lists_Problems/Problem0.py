047787a07ed83a30c7ec9e987ac0710fab2aca2e
Umair <umairgillani93@gmail.com>
2020-02-14 17:41:17 +0500

    singly list cycle check

--- Linked_Lists/Linked_Lists_Problems/Problem0.py
+++ Linked_Lists/Linked_Lists_Problems/Problem0.py
@@ -9,7 +9,6 @@ class SinglyNode(object):
     def __init__(self, value):
         self.value = value
         self.nextnode = None
-        self.prevnode = None
 
 first_node = SinglyNode(1)
 second_node = SinglyNode(2)
@@ -18,20 +17,22 @@ third_node = SinglyNode(3)
 first_node.nextnode = second_node
 second_node.nextnode = third_node
 
-first_node.prevnode = third_node
-
 print(first_node.value, second_node.value, third_node.value)
 print(first_node.nextnode.value)
 print(second_node.nextnode.value)
 
 def cycle_check(node):
-    # nodes = [first_node, second_node, third_node]
+    marker1 = node
+    marker2 = node
 
-    # for node in nodes:
-    if node.prevnode is None:
-        return False
-    else:
-        return True
+    marker1 = marker1.nextnode
+    marker2 = marker2.nextnode.nextnode
+
+    while marker2 != None and marker2.nextnode != None:
+         if marker1 == marker2:
+             return True
+             
+    return False
 
 is_cyclic = cycle_check(first_node)
 print(is_cyclic)
