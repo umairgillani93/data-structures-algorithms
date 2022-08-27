6032fe3574f7631e13e2536f12c9c0b941c12d6f
umairgillani93 <umairgillani93@gmail.com>
2022-08-28 03:53:31 +0500

    coding problem added

--- coding_problems/sub_array_kth_sum.py
+++ coding_problems/sub_array_kth_sum.py
@@ -1,9 +1,25 @@
+def _index(arr: list, item: int) -> int:
+  '''
+  Returns the item of index in an array
+  '''
+  for idx, val in enumerate(arr):
+    if val == item:
+      return idx
+
+assert _index([1,2,34], 34) == 2
 def sol(arr: list, k: int):
-  
+
   for i in range(len(arr)):
-    sum_ = sum(arr[i:])
-    print(sum_)
+    sum_  = sum(arr[i:])
+    if sum_ == k:
+      new_arr = arr[i:]
+
+      #return (arr.index(new_arr[0]), arr.index(new_arr[-1]))
+      return _index(arr, new_arr[0]), _index(arr, new_arr[-1])
+    else:
+      continue 
 
 
 if __name__ == '__main__':
+  print(sol([1,2,3,7,5], 12))
 
