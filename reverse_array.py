4621a0a0eef3898d4832f9904694e7979d276f5e
umairgillani93 <umairgillani93@gmail.com>
2022-08-28 02:11:16 +0500

    coding problem added

---
+++ reverse_array.py
@@ -0,0 +1,15 @@
+def sol(arr: list) -> list:
+  '''
+  Returns reversed array
+  time complexity: O(N)
+  space complexity: O(N)
+  '''
+
+  return [
+      arr[c] for c in range(len(arr) -1, -1, -1)
+      ]
+
+  return arr
+
+if __name__ == '__main__':
+  print(sol([1,2,3,4,5]))
